e8f4ccd62a27bbe558eb5621ea942541d95d80e4
Tanishksoam <soamtanishk@gmail.com>
2024-03-17 05:53:08 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import exercises from "./assets/data/exercises.json";
 
 export default function App() {
+  const exercise = exercises[0];
   return (
     <View style={styles.container}>
       <Text>Tanishk Soam is a millionaire by 2026</Text>
+      <View style={styles.exerciseContainer}>
+        <Text style={styles.exerciseTitle}>{exercise.name}</Text>
+        <Text style={styles.exerciseSubTitle}>
+          {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "gainsboro",
     alignItems: "center",
     justifyContent: "center",
+    padding: 10,
+  },
+  exerciseContainer: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    width: "100%",
+    gap: 5,
+  },
+  exerciseTitle: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  exerciseSubTitle: {
+    fontSize: 14,
+    fontWeight: "500",
   },
 });
